fe901d2e3888f11547a92cbd8afd735a9724df24
Mo Haoran <mo.haoran@pegasus.ae>
2019-08-28 20:50:25 +0400

    update data change default GPU visible

--- run_classifier.py
+++ run_classifier.py
@@ -31,7 +31,7 @@ from tqdm import tqdm
 from self_attention import SelfAttention2
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
 
 
