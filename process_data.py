86d2f29716010138058570c8cb3e979bb3774e43
Mo Haoran <mo.haoran@pegasus.ae>
2019-08-29 12:25:17 +0400

    update result, change to relative path

--- process_data.py
+++ process_data.py
@@ -529,24 +529,25 @@ def data_for_sentimental():
 
 
 def get_sentiment_result():
-    df = pd.read_csv(open("zhejiang/data_sentimental_old071/sentiment_ids.csv"), header=None)
+    import os
+    path_data_sentimental = "/Users/mo/Documents/github_projects/zhijiang/JNU/bert/zhejiang/data_sentimental"
+    df = pd.read_csv(open(os.path.join(path_data_sentimental,"sentiment_ids.csv"),encoding="GBK"), header=None)
     id_sentiment = {int(v): k for k, v in df.values}
     print(id_sentiment)
-    df = pd.read_csv(open("zhejiang/data_sentimental_old071/test_results.tsv", encoding="utf-8"), sep='\t', header=None)
+    df = pd.read_csv(open(os.path.join(path_data_sentimental,"test_results.tsv"), encoding="utf-8"), sep='\t', header=None)
     # print(df)
     sentiment = [id_sentiment[np.argmax(three)] for three in df.values]
     print(sentiment)
-    df = pd.read_csv(open("zhejiang/data_sentimental_old071/test.csv"), header=0)
+    df = pd.read_csv(open(os.path.join(path_data_sentimental,"test.csv"),encoding="GBK"), header=0)
     df["Polarities"] = sentiment
 
-
-    df_test = pd.read_csv(open(r"D:\projects_py\bert\data\zhejiang\th1\TEST\Test_reviews.csv", encoding="utf-8"), header=0)
+    df_test = pd.read_csv(open(os.path.join(path_data_sentimental,"Test_reviews.csv"), encoding="utf-8"), header=0)
 
     df = pd.merge(left=df_test, right=df, left_on="id", right_on="ID", how="left")
     df = df.fillna(value="_")
-    df.to_csv("zhejiang/data_sentimental_old071/纠正category错误/ner_sentiment_res.csv", index=False)
+    df.to_csv(os.path.join(path_data_sentimental,"ner_sentiment_res.csv"), index=False)
     df = df[["id", "AspectTerms", "Opinions", "Categories", "Polarities"]]
-    df.to_csv("zhejiang/data_sentimental_old071/纠正category错误/Result.csv", encoding="utf-8", header=None, index=False)
+    df.to_csv(os.path.join(path_data_sentimental,"Result.csv"), encoding="utf-8", header=None, index=False)
 
 
 if __name__ == '__main__':
@@ -572,5 +573,5 @@ if __name__ == '__main__':
     file_predict = r"D:\projects_py\bert\zhejiang\data_ner\label_test.txt"
     file_category_ids = r"D:\projects_py\bert\zhejiang\data_ner\category_ids.csv"
     # parse_ner_predict(file_predict, file_category_ids)
-    data_for_sentimental()
-    # get_sentiment_result()
+    # data_for_sentimental()
+    get_sentiment_result()
---
+++ zhejiang/haoran/readme_by_haoran.md
@@ -0,0 +1,18 @@
+
+# run docker on 12
+wget https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-12.zip
+unzip chinese_L-12_H-768_A-12.zip
+nvidia-docker run -it -v /data/haoran:/data/haoran 8e978d76921e /bin/bash
+
+cd bert
+python3 run.py -batch_size 16 -save_checkpoints_steps 1000 -save_summary_steps 500 -data_dir /data/haoran/aspect_based_sentiment/lc_branch/bert/zhejiang/data_ner -output_dir output -init_checkpoint /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/bert_model.ckpt -bert_config_file /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/bert_config.json -vocab_file /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/vocab.txt -device_map 0
+
+
+python3 run_classifier.py -batch_size 16 -max_seq_length 128 -do_train False -do_eval False -do_predict True -save_checkpoints_steps 1000 -save_summary_steps 500 -data_dir /data/haoran/aspect_based_sentiment/lc_branch/bert/zhejiang/data_sentimental -output_dir zhejiang/output_sentiment -init_checkpoint /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/bert_model.ckpt -bert_config_file /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/bert_config.json -vocab_file /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/vocab.txt -device_map 0
+python3 run_classifier.py -max_seq_length 128 -do_train False -do_eval False -do_predict True -data_dir /data/haoran/aspect_based_sentiment/lc_branch/bert/zhejiang/data_sentimental -output_dir zhejiang/output_sentiment -init_checkpoint /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/bert_model.ckpt -bert_config_file /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/bert_config.json -vocab_file /data/haoran/aspect_based_sentiment/bert/chinese_L-12_H-768_A-12/vocab.txt
+
+docker run -p 8501:8501 --mount type=bind,source=/Users/mo/Documents/gitlab_projects/bert-bilstm-crf-ner/predict_optimizer/ner_model/,target=/models/ner_model -e MODEL_NAME=ner_model -t tensorflow/serving
+
+
+scp haoran@10.1.129.12:/data/haoran/aspect_based_sentiment/lc_branch/bert/output/label_test.txt ./
+scp haoran@10.1.129.12:/data/haoran/aspect_based_sentiment/lc_branch/bert/output/label2id.pkl ./
